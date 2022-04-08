93777a892b0bfa27bbb5cf1730578e54e4c7792e
thanhnguyen46 <phuocthanhnguyen46@gmail.com>
2022-04-08 01:08:55 -0400

    Update gameContent.js
    Remove unnecessary code

--- js/gameContent.js
+++ js/gameContent.js
@@ -1,30 +1,11 @@
 function gameContent() {
 
 var content = `
-        <style>
-            p {
-                margin-left: 1.5rem;
-            }
-            .flexContainer {
-                display:flex; 
-                flex-direction: row;
-                background-color: #edd5d5;
-            }
-            .flexContainer .item {
-                width: 100%;
-                box-sizing: border-box; /* makes padding and border counted in the width */
-            }
-        </style>
-        <h3>JS Object HW</h3>
-        <p>
-            Test
-        </p>
     `;  
         var ele = document.createElement("div");
         ele.innerHTML = content; // the HTML code specified just above...
 
         var gameDiv = document.createElement("div");
-        gameDiv.classList.add('flexContainer');
         ele.appendChild(gameDiv);
         
         // CREATE ITEM 1
